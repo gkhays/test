10628b479f619682b3f3ffce97a34228bf7ae1e8
Garve Hays <gkhays@gmail.com>
2020-06-24 18:02:29 -0500

    Updates to local hook scripts

--- git-hooks/run-eslint.sh
+++ git-hooks/run-eslint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-exec eslint .
+repodir="${1}"
+
+git status --porcelain | grep '^[A|M]' | cut -c 4- | grep -e '.*\.js$' | while read f
+do
+    filename="${f}"
+    eslint "$filename"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+done
